2e02344affb0838d18b67d45e1f19ba332cb5f54
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-14 14:05:30 -0700

    Update UserIOConsoleImpl.java

--- VendingMachine/src/main/java/ui/UserIOConsoleImpl.java
+++ VendingMachine/src/main/java/ui/UserIOConsoleImpl.java
@@ -9,6 +9,54 @@ package ui;
  *
  * @author chadb
  */
-public class UserIOConsoleImpl {
+import java.util.Scanner;
+
+/**
+ *
+ * @author chadb
+ */
+public class UserIOConsoleImpl implements UserIO {
+     final private Scanner console = new Scanner(System.in);
+     
+    @Override
+    public void print(String msg) {
+        System.out.println(msg);
+    }
     
+    @Override
+    public int readInt(String msgPrompt, int min, int max) {
+        int result;
+        do {
+            result = readInt(msgPrompt);
+        } while (result < min || result > max);
+
+        return result;
+    }
+    
+    @Override
+    public int readInt(String msgPrompt) {
+        boolean invalidInput = true;
+        int num = 0;
+        while (invalidInput) {
+            try {
+                // print the message msgPrompt (ex: asking for the # of cats!)
+                String stringValue = this.readString(msgPrompt);
+                // Get the input line, and try and parse
+                num = Integer.parseInt(stringValue); // if it's 'bob' it'll break
+                invalidInput = false; // or you can use 'break;'
+            } catch (NumberFormatException e) {
+                // If it explodes, it'll go here and do this.
+                this.print("Input error. Please try again.");
+            }
+        }
+        return num;
+    }
+    
+    @Override
+    public String readString(String msgPrompt) {
+        System.out.println(msgPrompt);
+        return console.nextLine();
+    }
+
+
 }
