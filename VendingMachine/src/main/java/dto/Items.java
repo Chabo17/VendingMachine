0f601ece6227c2dd3ed642c4a91a9d0c7e4f686e
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-14 13:06:52 -0700

    fresh files

---
+++ VendingMachine/src/main/java/dto/Items.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author chadb
+ */
+public class Items {
+    private String name;
+    private double price;
+    private int count;
+    
+    public Items(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+    
+}
