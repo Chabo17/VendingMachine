6a822c1b7a1da2d7ebebef900c57f3a2b6dd4bf1
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-15 07:28:04 -0700

    Update App.java

--- VendingMachine/src/main/java/app/App.java
+++ VendingMachine/src/main/java/app/App.java
@@ -5,10 +5,34 @@
  */
 package app;
 
+import controller.Controller;
+import dao.VendingMachineDao;
+import dao.VendingMachineFileImpl;
+import ui.UserIO;
+import ui.UserIOConsoleImpl;
+import ui.VendingMachineView;
+
 /**
  *
  * @author chadb
  */
 public class App {
+             public static void main(String[] args) {
+            // Instantiate the UserIO implementation
+        UserIO myIo = new UserIOConsoleImpl();
+        // Instantiate the View and wire the UserIO implementation into it
+        VendingMachineView myView = new VendingMachineView(myIo);
+        // Instantiate the DAO
+        VendingMachineDao myDao = new VendingMachineFileImpl();
+        // Instantiate the Audit DAO
+        //VendingMachineAuditDao myAuditDao = new VendingMachineAuditDaoFileImpl();
+        // Instantiate the Service Layer and wire the DAO and Audit DAO into it
+        //VendingMachineServiceLayer myService = new VendingMachineServiceLayerImpl(myDao, myAuditDao);
+        // Instantiate the Controller and wire the Service Layer into it
+        Controller controller = new Controller(myView);
+        // Kick off the Controller
+        controller.run();
+        
+        }
     
 }
